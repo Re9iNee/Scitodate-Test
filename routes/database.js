dd42438bf2e44c9e5b98b5c1e5d5d30d770b296d
Re9iNee <attarzadeh76@gmail.com>
2021-09-02 20:27:26 +0430

    feature: upload json file. added formidable to database api

--- routes/database.js
+++ routes/database.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const formidable = require("formidable");
 const router = express.Router();
 
-
-
 router.post("/", (req, res) => {
-  res.send(req.body);
+  // TODO: use Paths module to define uploadDir
+  // recommended: max file size: 2mb
+  const form = formidable({
+    multiples: false,
+    keepExtensions: true,
+    // maxFileSize: 2 * 1024 * 1024,
+    uploadDir: "./uploads",
+  });
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.log("error", err);
+      return;
+    }
+    res.status(200).json({ fields, files });
+  });
 });
 
 module.exports = router;

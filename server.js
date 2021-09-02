feb86b3dba1941df8e2e1a591a34b6f50cd2982a
Re9iNee <attarzadeh76@gmail.com>
2021-09-02 19:26:49 +0430

    setup database route

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 
-require('dotenv').config();
+// Best to set a file limit on it express.json({limit: '1mb'})
+app.use(express.json());
+
+require("dotenv").config();
 
 const port = process.env.SERVER_PORT || 5000;
 app.listen(port, () => console.log(`Listening on ${port}`));
 
+app.get("/", (req, res) => {
+  res.send("ITS OK MAN");
+});
 
-app.get('/', (req, res) => {
-    res.send("ITS OK MAN")
-})
+// Import Routes
+const databaseRoute = require("./routes/database");
+app.use("/database", databaseRoute);

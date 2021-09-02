175813355c9a042fa5724dff25f32426d7eb1dbe
Re9iNee <attarzadeh76@gmail.com>
2021-09-02 19:48:00 +0430

    write a simple form

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <form action="/database" enctype="multipart/form-data" method="post">
+        <div>
+          Text field title: <input type="text" name="title" />
+        </div>
+        <div>
+          File:{" "}
+          <input type="file" name="someExpressFiles"/>
+        </div>
+        <input type="submit" value="Upload" />
+      </form>
     </div>
   );
 }

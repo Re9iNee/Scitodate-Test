d98349df36d6d9dfcd156ba563f24494102dfc65
Re9iNee <attarzadeh76@gmail.com>
2021-09-03 22:00:32 +0430

    mofiy Authors.js list of all Authors

--- client/src/components/Authors.js
+++ client/src/components/Authors.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 export default class Authors extends React.Component {
-    render() {
-        return <pre>This is Authors Profile</pre>
-    }
+  render() {
+    return (
+    <div>
+        <h1>Authors</h1>
+        <h5>List of All Authors here</h5>
+        <input type="text" placeholder="search through Authors"/>
+        <ul>
+            <li>Wong</li>
+            <pre>Affilitaion</pre>
+        </ul>
+        <ul>
+            <li>Wong</li>
+            <pre>Affilitaion</pre>
+        </ul>
+    </div>
+    );
+  }
 }

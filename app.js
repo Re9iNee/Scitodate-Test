b8d16fdb2eb0365c85e6759da64485ca45f26153
Re9iNee <attarzadeh76@gmail.com>
2021-09-03 17:08:25 +0430

    Processing Data : #2 Processing and adding Papers to Database

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const { readFileSync } = require("fs");
 const Authors = require("./models/authors");
+const Papers = require("./models/papers");
+const { nanoid } = require("nanoid");
 
-function processData(path) {
+async function processData(path) {
   const datas = JSON.parse(readFileSync(path, { encoding: "utf-8" }));
 
   let allAuthors = new Array();
@@ -31,11 +33,55 @@ function processData(path) {
   }
   //   TODO: remove duplicates from allAuthors. (currently handled by mongoDB)
   //   Save to DB
-  Authors.insertMany([...allAuthors], { ordered: false })
-    .then((resp) => {
-      console.log(resp);
-    })
-    .catch((err) => console.log("myError  ", err));
+  try {
+    const resp = await Authors.insertMany([...allAuthors], {
+      ordered: false,
+    });
+    console.log("Inserting Authors: ", resp);
+  } catch (err) {
+    console.log("Inserting Authors: ", err);
+  }
+
+  // NOTE: is there a way to find in Authors locally? cause we would upload once. without any modifications to Authors Papers, and we would do that locally
+  // Process Papers
+  let allPapers = new Array();
+  for (let data of datas) {
+    let authIDs = new Array();
+    for (let author of data.authors) {
+      authIDs.push(author._id);
+    }
+
+    // Generate newId
+    const paperId = nanoid();
+    //   Assign this paper to Authors
+    for (let AuthId of authIDs) {
+      try {
+        const resp = await Authors.findByIdAndUpdate(AuthId, {
+          $push: { papers: `${paperId}` },
+        }).exec();
+        console.log("Updating Authors: ", resp);
+      } catch (err) {
+        console.log("Updating Authors: ", err);
+      }
+    }
+    const newPaper = new Papers({
+      _id: paperId,
+      date: data.date,
+      topic_scores: [...data.topic_scores],
+      topics: [...data.topics],
+      abstract: data.abstract,
+      title: data.title,
+      authors: [...authIDs],
+    });
+    allPapers.push(newPaper);
+  }
+  //   Save to DB
+  try {
+    const resp = await Papers.insertMany([...allPapers], { ordered: false });
+    console.log("Inserting Papers: ", resp);
+  } catch (err) {
+    console.log("Inserting Papers: ", err);
+  }
 }
 
 module.exports = { processData };

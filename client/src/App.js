78e243d9d44ae54d4191f386faa3aee0dc7f8d8d
Re9iNee <attarzadeh76@gmail.com>
2021-09-03 21:05:01 +0430

    defined React Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,34 @@
-function App() {
-  return (
-    <div>
-      <form action="/database" enctype="multipart/form-data" method="post">
-        <div>
-          Text field title: <input type="text" name="title" />
-        </div>
-        <div>
-          File:{" "}
-          <input type="file" name="dataFile"/>
-        </div>
-        <input type="submit" value="Upload" />
-      </form>
-    </div>
-  );
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/authors">
+            <Authors />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+function Admin() {
+  return <pre>This is Admin Area</pre>
+}
+function Authors() {
+  return <pre>This is Authors List</pre>
+}
+function Home() {
+  return <pre>This is Home? </pre>
 }
 
 export default App;

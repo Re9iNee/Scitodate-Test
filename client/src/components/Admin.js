be9e1b2811cab8032d57cccf58fefaead52f95a1
Re9iNee <attarzadeh76@gmail.com>
2021-09-03 21:54:38 +0430

    added form to upload file (Admin Area)

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -2,6 +2,15 @@ import React from "react";
 
 export default class Admin extends React.Component {
   render() {
-    return <pre>This is Admin Area</pre>;
+    return (
+      <form action="/database" enctype="multipart/form-data" method="post">
+        <h1>Upload your data file here</h1>
+        <pre>.json format</pre>
+        <div>
+          File: <input type="file" name="dataFile" />
+        </div>
+        <input type="submit" value="Upload" />
+      </form>
+    );
   }
 }

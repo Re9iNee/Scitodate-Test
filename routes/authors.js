b56763779b35ea1c313016290037ec3d1298fc95
Re9iNee <attarzadeh76@gmail.com>
2021-09-04 21:28:53 +0430

    author express route - return all coAuthors that worked with specific Author :id

--- routes/authors.js
+++ routes/authors.js
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
   try {
     const result = await Authors.find();
     if (!result) {
-        throw("something amiss, check server console")
+      throw Error("something amiss, check server console");
     }
     res.status(200).json(result);
   } catch (err) {
@@ -20,7 +20,20 @@ router.get("/:id", async (req, res) => {
   try {
     const result = await Authors.findById(req.params.id);
     if (!result) {
-        throw("couldn't find this author")
+      throw Error("couldn't find this author");
+    }
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ msg: "failed", error: err });
+  }
+});
+
+router.get("/coAuthors/:id", async (req, res) => {
+  // return all co-authors with this specific author :id
+  try {
+    const result = await Authors.find({ coAuthors: Number(req.params.id) });
+    if (!result) {
+      throw Error("couldn't find coAuthors");
     }
     res.status(200).json(result);
   } catch (err) {

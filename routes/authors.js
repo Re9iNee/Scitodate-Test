23e1a8466226d79a6cf65c8f872e3eede31adfad
Re9iNee <attarzadeh76@gmail.com>
2021-09-04 13:42:44 +0430

    author route - error handling

--- routes/authors.js
+++ routes/authors.js
@@ -6,6 +6,9 @@ router.get("/", async (req, res) => {
   // return all authors
   try {
     const result = await Authors.find();
+    if (!result) {
+        throw("something amiss, check server console")
+    }
     res.status(200).json(result);
   } catch (err) {
     res.status(400).json({ msg: "failed", error: err });
@@ -16,6 +19,9 @@ router.get("/:id", async (req, res) => {
   // return author with the id of :id
   try {
     const result = await Authors.findById(req.params.id);
+    if (!result) {
+        throw("couldn't find this author")
+    }
     res.status(200).json(result);
   } catch (err) {
     res.status(400).json({ msg: "failed", error: err });

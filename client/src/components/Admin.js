8ab6d4d39b8b9e7616d71a15bd49c45749100093
Re9iNee <attarzadeh76@gmail.com>
2021-09-05 19:24:22 +0430

    disable upload button when input is not valid

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,29 +1,45 @@
 import React from "react";
 
 export default class Admin extends React.Component {
-  submit = (ev) => {
-    // ev.preventDefault();
-    // TODO: local validation here
+  constructor(props) {
+    super(props);
+    this.state = {
+      disabled: true,
+    };
+  }
+  handleFileChange = (ev) => {
+    if (
+      ev.target.files.length &&
+      ev.target.files[0].type === "application/json"
+    ) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
   };
   render() {
     return (
       <form
         action="/database"
-        enctype="multipart/form-data"
+        encType="multipart/form-data"
         method="post"
         className="container"
         onSubmit={this.submit}
       >
         <h1>Upload your data file</h1>
-        <p className="large-p text-secondary subhead">.json format</p>
-        <div>
-          File: <input type="file" name="dataFile" />
+        <p className="small-p text-secondary subhead">
+          you can upload your .json file here.
+        </p>
+        <div className="input-container medium-p">
+          File:{" "}
+          <input type="file" name="dataFile" onChange={this.handleFileChange} />
         </div>
         <input
           id="uploadBtn"
           className="large-p"
           type="submit"
           value="Upload"
+          disabled={this.state.disabled}
         />
       </form>
     );

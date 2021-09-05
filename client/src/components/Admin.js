3f50d8e84b0aa2d8b0c8dd80f4871521cf3e11f0
Re9iNee <attarzadeh76@gmail.com>
2021-09-05 18:38:56 +0430

    admin component - styling component

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,15 +1,30 @@
 import React from "react";
 
 export default class Admin extends React.Component {
+  submit = (ev) => {
+    // ev.preventDefault();
+    // TODO: local validation here
+  };
   render() {
     return (
-      <form action="/database" enctype="multipart/form-data" method="post">
-        <h1>Upload your data file here</h1>
-        <pre>.json format</pre>
+      <form
+        action="/database"
+        enctype="multipart/form-data"
+        method="post"
+        className="container"
+        onSubmit={this.submit}
+      >
+        <h1>Upload your data file</h1>
+        <p className="large-p text-secondary subhead">.json format</p>
         <div>
           File: <input type="file" name="dataFile" />
         </div>
-        <input type="submit" value="Upload" />
+        <input
+          id="uploadBtn"
+          className="large-p"
+          type="submit"
+          value="Upload"
+        />
       </form>
     );
   }

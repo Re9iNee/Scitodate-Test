8b13f017006de13c28109eb5e5425bba64b6aed0
Re9iNee <attarzadeh76@gmail.com>
2021-09-05 21:32:07 +0430

    modify home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,18 @@
-import React from "react"
+import React from "react";
 
 export default class Home extends React.Component {
-    render() {
-        return <pre>Welcome Home!</pre>
-    }
+  render() {
+    return (
+      <div>
+        <h1>Welcome Home!</h1>
+        <p>
+          You can head over to: <a href="/admin">Admin area</a> to upload data
+          file.
+        </p>
+        <p>
+          or <a href="/authors">See all authors list</a>
+        </p>
+      </div>
+    );
+  }
 }
